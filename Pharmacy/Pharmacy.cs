0e40b3db43a1802152fdd02a3e178a35a70e1f93
minniunicorn <nknknkf92@gmail.com>
2022-02-17 09:32:28 +0300

    Добавьте файлы проекта.

---
+++ Pharmacy/Pharmacy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SR_1
+{
+    class Pharmacy
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
