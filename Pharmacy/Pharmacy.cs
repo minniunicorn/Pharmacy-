da4473ac2834f3cbbc566abdb253b5cd9383308b
minniunicorn <nknknkf92@gmail.com>
2022-02-22 23:36:46 +0300

    Класс Аптека

--- Pharmacy/Pharmacy.cs
+++ Pharmacy/Pharmacy.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace SR_1
+namespace Classes
 {
-    class Pharmacy
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Класс Аптека");
-            List<PharmacyClass> aspirin = new List<PharmacyClass>;
+            
+            string name1 = "ProstoApteka";
+            List<PharmacyClass> lecar_name1 = new List<PharmacyClass>();
+            lecar_name1.Add(new PharmacyClass("Aspirin", 100));
+            lecar_name1.Add(new PharmacyClass("Pentalgin", 200));
+            PharmacyClass.Info(name1, lecar_name1);
+            
+            Console.WriteLine();
+            
+            Console.WriteLine("Название Вашей аптеки:");
+            string name2 = Console.ReadLine();
+            int test = 1;
+            List<PharmacyClass> lecar_name2= new List<PharmacyClass>();
+            while (test != 0)
+            {
+                Console.WriteLine("Введите название лекарства:");
+                string lecarstvo = Console.ReadLine();
+                Console.WriteLine("Введите его цену:");
+                int price = int.Parse(Console.ReadLine());
+                lecar_name2.Add(new PharmacyClass(lecarstvo, price));              
+                Console.WriteLine("Есть еще лекарства? 1-да, 0-нет");
+                test = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            PharmacyClass.Info(name2, lecar_name2);
 
+
+            Console.ReadKey();
         }
     }
 }
--- Pharmacy/PharmacyClass.cs
+++ Pharmacy/PharmacyClass.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-public class Pharmacy
+public class PharmacyClass
 {
     /// <summary>
     /// Название аптеки
@@ -19,9 +19,33 @@ public class Pharmacy
     /// Цена лекарства
     /// </summary>
     public int price;
-    public Pharmacy(string lecarstvo, int price)
+    public PharmacyClass(string lecarstvo, int price)
     {
         this.lecarstvo = lecarstvo;
         this.price = price;
     }
+    /// <summary>
+    /// Список лекарст с ценами
+    /// </summary>
+    public List<PharmacyClass> lecar_name = new List<PharmacyClass>();
+    public static void Show(string name)
+    {
+        Console.WriteLine(name);
+    }
+    public static void Info(string name, List<PharmacyClass> lecar_name)
+    {
+        Console.WriteLine("Название: " + name);
+        Console.WriteLine("Список лекарств:");
+        foreach (PharmacyClass z in lecar_name)
+        {
+            Console.WriteLine(z.lecarstvo + " " + z.price);
+        }
+    }
+    public static int MaxPrice(List<PharmacyClass>lecar_name)
+    {     
+        int size = lecar_name.Count;
+        int max = lecar_name.Max(point => point.price);
+        return max;
+    }
+    
 }
